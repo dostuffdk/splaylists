d2450c5c0772e8eb83b61ea8e2b83c01dce24bfa
Anders Mogensen <anders@dostuff.dk>
2018-04-25 20:28:41 +0200

    Server handling.

--- src/App.js
+++ src/App.js
@@ -138,7 +138,8 @@ class App extends Component {
           {playlistsToRender.map(playlist =>
             <Playlist playlist={playlist} />
           )}
-        </div> : <button onClick={() => window.location='http://localhost:8888/login' }
+        </div> : <button onClick={() => {
+          window.location = window.location.href.includes('localhost') ? 'http://localhost:8888/login' : 'https://splaylists-backend.herokuapp.com/login' }}
           style={{padding: '20px', 'font-size': '50px', 'margin-top': '20px'}}>Sign in with Spotify</button>
         }
       </div>
