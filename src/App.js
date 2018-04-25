988fe42b7e08b55b993659b4ed95f0b5b0979f40
Anders Mogensen <anders@dostuff.dk>
2018-04-25 20:22:42 +0200

    Early return if no access token, so we can log in.

--- src/App.js
+++ src/App.js
@@ -85,7 +85,10 @@ class App extends Component {
   }
 
   componentDidMount() {
-    let accessToken = new URLSearchParams(window.location.search).get('access_token')
+    let accessToken = new URLSearchParams(window.location.search).get('access_token');
+    if (!accessToken) {
+      return;
+    }
 
     fetch('https://api.spotify.com/v1/me', {
       headers: {
